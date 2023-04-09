e5098bcc78eab2a00956cd8d0bfc4e2fe8416b62
Aashish <aashish.pokhrel01@gmail.com>
2023-04-09 21:55:25 +0545

    This is first commit

---
+++ timedelta.py
@@ -0,0 +1,7 @@
+from datetime import datetime , timedelta
+noww = datetime.now()
+after = timedelta(days=30)
+print("Your registration will be expired in :", noww + after)
+
+a = datetime.date(may: 12) -> _
+datetime.date()
